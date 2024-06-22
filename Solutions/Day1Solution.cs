eec67487b205a63c9baa6652a664b5c9c4fa8353
Gessio Mori Neto <gessiomorin@gmail.com>
2024-06-22 10:38:06 -0300

    Day1AB

---
+++ Solutions/Day1Solution.cs
@@ -0,0 +1,37 @@
+﻿using Main.Tools;
+
+namespace Main.Solutions
+{
+    public class Day1Solution : ISolution
+    {
+        public string RunPartA(string[] inputData)
+        {
+            long total = 0;
+
+            foreach (string line in inputData)
+            {
+                total += int.Parse(line) / 3 - 2;
+            }
+
+            return total.ToString();
+        }
+
+        public string RunPartB(string[] inputData)
+        {
+            long total = 0;
+
+            foreach (string line in inputData)
+            {
+                int aditionalFuel = int.Parse(line) / 3 - 2;
+
+                while (aditionalFuel > 0)
+                {
+                    total += aditionalFuel;
+                    aditionalFuel = aditionalFuel / 3 - 2;
+                }
+            }
+
+            return total.ToString();
+        }
+    }
+}
