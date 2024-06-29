5783ff4b4c99a91df54638d728baa67d48a7ea0e
Gessio Mori Neto <gessiomorin@gmail.com>
2024-06-29 10:08:13 -0300

    Day5AB

---
+++ Solutions/Day6Solution.cs
@@ -0,0 +1,17 @@
+﻿using Main.Tools;
+
+namespace Main.Solutions
+{
+    public class Day6Solution : ISolution
+    {
+        public string RunPartA(string[] inputData)
+        {
+            return "A";
+        }
+
+        public string RunPartB(string[] inputData)
+        {
+            return "B";
+        }
+    }
+}
